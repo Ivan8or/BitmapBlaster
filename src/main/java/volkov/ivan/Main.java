845b6a0b46a9f8abf73db1227299f6c093163d1e
Ivan8or <ivanthegreat825@gmail.com>
2023-12-22 20:41:31 -0500

    initial commit

---
+++ src/main/java/volkov/ivan/Main.java
@@ -0,0 +1,11 @@
+package volkov.ivan;
+
+import volkov.ivan.ui.GUI;
+
+public class Main {
+    public static void main(String[] args) {
+
+        GUI gui = new GUI();
+
+    }
+}
---
+++ src/main/java/volkov/ivan/ui/GUI.java
@@ -0,0 +1,11 @@
+package volkov.ivan.ui;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+
+    public GUI() {
+        this.setSize(100,200);
+        this.setVisible(true);
+    }
+}
